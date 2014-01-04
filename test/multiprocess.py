69bd75aed578e08108566d0fd874bd7c67e88672
Christopher O'Brien <cobrien@redhat.com>
2014-01-03 19:01:53 -0500

    still not a proper test.

--- test/multiprocess.py
+++ test/multiprocess.py
@@ -7,6 +7,7 @@ def work(i):
     sleep(1)
     print "Hello World: Worker ID %i" % i
     sleep(5)
+    print "Done with work: %i" % i
     return i
 
 
@@ -14,26 +15,49 @@ def broke(i):
     sleep(1)
     print "Wello Horld: Broken Worker ID %i" % i
     sleep(5)
+    print "herp derp: %i" % i
     raise Exception("tummy ache")
 
 
 with pmp.ProxyPromising() as p:
     val_broken = p.promise(partial(broke, 10))
+    print "promised val_broken"
 
     val2 = p.promise(partial(work, 2))
+    print "promised val2"
+    
     val3 = p.promise(partial(work, 3))
+    print "promised val3"
+
     val4 = p.promise(partial(work, 4))
+    print "promised val4"
+
     val5 = p.promise(partial(work, 5))
+    print "promised val5"
+
     val6 = p.promise(partial(work, 6))
+    print "promised val6"
+
     val7 = p.promise(partial(work, 7))
+    print "promised val7"
+
+print "left managed scope"
 
 
-print "val7 is", val7
-print "val6 is", val6
-print "val5 is", val5
-print "val4 is", val4
-print "val3 is", val3
 print "val2 is", val2
+print "val3 is", val3
+print "val4 is", val4
+print "val5 is", val5
+print "val6 is", val6
+print "val7 is", val7
 
-print "val_broken is", val_broken
+try:
+    print "val_broken is", val_broken
+except Exception, e:
+    print e
+
+try:
+    print "val_broken is", val_broken
+except Exception, e:
+    print e
 
